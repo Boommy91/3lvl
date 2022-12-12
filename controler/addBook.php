6f1699fb858673e7d7a092d86d5378697429c6e2
Fujitsu <igornechay91@gmail.com>
2022-12-12 20:05:02 +0200

    start forms

---
+++ controler/addBook.php
@@ -0,0 +1,11 @@
+<?php
+$db = require 'connect.php';
+$name = $_POST['nameOfBook'];
+$year = $_POST['year'];
+$authorFirst = $_POST['authorFirst'];
+$authorSecond = $_POST['authorSecond'];
+$authorThird = $_POST['authorThird'];
+$description = $_POST['description'];
+$imgSrc = $_POST['img'];
+$sql = "INSERT INTO books (`name`,`year`,`author_1`,`author_2`,`author_3`,`description`,`img_src`) VALUES ('$name',$year,'$authorFirst','$authorSecond','$authorThird','$description','$imgSrc')";
+$db->exec($sql);
--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-$("#add").click(function () {
+$("#addButton").click(function () {
     let nameOfBook = $("#nameBook").val();
     let year = $("#yearBook").val();
     let authorFirst = $("#authorBook1").val();
     let authorSecond = $("#authorBook2").val();
     let authorThird = $("#authorBook3").val();
-    let description = $("#someTxt").val();
-    let imgName = $("#description").val();
+    let description = $("#description").val();
+    let img = $("#imgBook").val();
+
     $.ajax({
-        url: 'addBook.php',
+        url: 'controler/addBook.php',
         type: 'post',
-        data: {nameOfBook, year, authorFirst, authorSecond, authorThird, description,imgName},
-        success: function (data) {
-            alert(data)
-        }
+        data: {nameOfBook, year, authorFirst, authorSecond, authorThird, description,img},
     })
 });
+
